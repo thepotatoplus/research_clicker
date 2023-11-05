c86c2ce81e375c7b3d0dd38306f3ad633bd61076
the potato <potatoplusplus@gmail.com>
2023-11-04 22:48:02 -0400

    i moved the image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
+<img src='/potatoplusplus.svg' width = 64 height = 64> 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<img src='/potatoplusplus.svg'>
